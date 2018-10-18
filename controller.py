46e14f788d2293c9d34a9c7dc5832b17797bea52
liuyun <zhheng.com@gmail.com>
2018-10-19 00:46:09 +0800

    add db

--- controller.py
+++ controller.py
@@ -3,28 +3,22 @@
 import time
 from mongodb import *
 from wcrawler import *
+from mysql import *
 import random
 import sys
 
 
 def get_crawler():
+    list = get_cookies()
     return [WCrawler(
-                cookie='SCF=Ak0iONoGebfHR0NAtTW0CmAZx6EjcwU03z5VAc3lw-EV2_vqH1RWltCEZ9cb3QUtsyVIrqLe49jZQ_YemVADSEw.; SUHB=0cS3DxJFGoM6z_; _T_WM=d2f6835d079626b49080c098489fb2d3; SSOLoginState=1539784958; ALF=1542375494; SUB=_2A252wzCuDeRhGeVP4lUX-CfMyT2IHXVSTFDmrDV6PUJbktAKLVTgkW1NTTnhI5zuavezQaLEG5W0DB558BsG18I4; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WFyflfjSq9Cbw_FHi695g4y5JpX5K-hUgL.Foep1KMc1h.7eo22dJLoI7yoSJn4P0Mfe7tt', \
+                cookie=cookie, \
                 max_num_weibo=0, \
                 max_num_fans=20, \
                 max_num_follow=20, \
                 max_num_page=2, \
                 wfilter='all', \
-                return_type='json'),
-            WCrawler(
-                cookie='_T_WM=97ea6d6559090059dc838f6fd8f3c0bd; ALF=1542037139; SCF=AmgqUtXVZtz-IzMlC2KvS0q5yUyW75FdldFmJc9-LTOJOAGtm6lES5EulseTinzZ3ijFxUD0AXbv8FSP-gHdI-A.; SUB=_2A252xmHMDeRhGeBJ7VcQ9ybJyTmIHXVSSQ-ErDV6PUJbktANLRXZkW1NRilphGJnUEN39rursTrHdx7ggQdalgno; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WFeZhPUbIIUONDmcPy9uRfo5JpX5K-hUgL.FoqNSo-pS0nfeo-2dJLoIpRLxK-LBo5L12qLxKML12eLBoBLxK-LB-BL1KWbqJqt; SUHB=0G0-GGcPqvU1-A; SSOLoginState=1539445148', \
-                max_num_weibo=0, \
-                max_num_fans=20, \
-                max_num_follow=20, \
-                max_num_page=2, \
-                wfilter='all', \
-                return_type='json')
-    ]
+                return_type='json') for cookie in list
+            ]
 
 
 if __name__ == '__main__':
@@ -55,5 +49,5 @@ if __name__ == '__main__':
                     crawler = crawlers.pop()
             finally:
                 print("[" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + "] save...")
-        time.sleep(random.randint(4, 9))
+        time.sleep(random.randint(3, 6))
     pass
--- mysql.py
+++ mysql.py
@@ -1,2 +1,24 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+import pymysql
+from wcrawler import *
+
+
+# 数据库查询cookie
+
+def get_cookies():
+    # 打开数据库连接
+    db = pymysql.connect("localhost", "root", "mysql123", "weibo")
+    # 使用 cursor() 方法创建一个游标对象 cursor
+    cursor = db.cursor()
+
+    # 使用 execute()  方法执行 SQL 查询
+    cursor.execute("SELECT cookie FROM auth")
+
+    # 使用 fetchone() 方法获取单条数据.
+    data = cursor.fetchall()
+    # 关闭数据库连接
+    db.close()
+
+    return [cookie[0] for cookie in data]
+
